b35b0e586ccd3260f75449e9f4f10a12319bdd32
Edzo <edzo@edzo.dev>
2021-01-03 17:55:09 +0400

    Add India and make map zoomable

--- src/MapChart.js
+++ src/MapChart.js
@@ -9,36 +9,38 @@ import {
 const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
 const markers = []
-const countriesVisited = ["NLD", "DEU", "POL", "FR", "DNK", "KWT", "ARE", "TUR", "ROU", "HUN", "SWE", "NOR", "USA", "GBR", "ESP", "FRA", "CHE", "AUT", "ITA", "BEL", "SAU"]
+const countriesVisited = ["NLD", "DEU", "POL", "FR", "DNK", "IND", "KWT", "ARE", "TUR", "ROU", "HUN", "SWE", "NOR", "USA", "GBR", "ESP", "FRA", "CHE", "AUT", "ITA", "BEL", "SAU", "LUX"]
 const MapChart = () => {
     return (
         <ComposableMap>
-            <Geographies geography={geoUrl}>
-                {({ geographies }) =>
-                    geographies.map((geo) => {
-                        // console.log(geo);
-                        return (
-                            <Geography
-                                key={geo.rsmKey}
-                                geography={geo}
-                                fill={countriesVisited.includes(geo.properties.ISO_A3) ? '#FF5A00' : '#AEAEAE'}
-                            />
-                        );
-                    })
-                }
-            </Geographies>
-            {markers.map(({ name, coordinates, markerOffset }) => (
-                <Marker key={name} coordinates={coordinates}>
-                    <circle r={10} fill="#F00" stroke="#fff" strokeWidth={2} />
-                    <text
-                        textAnchor="middle"
-                        y={markerOffset}
-                        style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
-                    >
-                        {name}
-                    </text>
-                </Marker>
-            ))}
+            <ZoomableGroup zoom={1}>
+                <Geographies geography={geoUrl}>
+                    {({ geographies }) =>
+                        geographies.map((geo) => {
+                            // console.log(geo);
+                            return (
+                                <Geography
+                                    key={geo.rsmKey}
+                                    geography={geo}
+                                    fill={countriesVisited.includes(geo.properties.ISO_A3) ? '#FF5A00' : '#AEAEAE'}
+                                />
+                            );
+                        })
+                    }
+                </Geographies>
+                {markers.map(({ name, coordinates, markerOffset }) => (
+                    <Marker key={name} coordinates={coordinates}>
+                        <circle r={10} fill="#F00" stroke="#fff" strokeWidth={2} />
+                        <text
+                            textAnchor="middle"
+                            y={markerOffset}
+                            style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+                        >
+                            {name}
+                        </text>
+                    </Marker>
+                ))}
+            </ZoomableGroup>
         </ComposableMap>
     );
 };
