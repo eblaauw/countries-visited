6d0bb754bd738fb328bd408075312d3274cc2923
eb <edzoblaauw@gmail.com>
2021-08-12 13:35:56 +0200

    Add tailwindcss

--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ import MapChart from "./MapChart";
 function App() {
   const [content, setContent] = useState("");
   return (
-    <div>
-      <MapChart setTooltipContent={setContent} />
-  
+    <div className="container mx-auto px-4 md:px-0 font-sans lg:w-3/4">
+      <div className="mt-12 md:mt-20">
+        <h1 class="text-5xl md:text-6xl font-bold relative font-title">Countries I've visited</h1>
+        <div>
+          <MapChart setTooltipContent={setContent} />
+        </div>
+      </div>
     </div>
   );
 }
